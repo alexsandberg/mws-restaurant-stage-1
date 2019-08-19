04543d916a75df14470662d73751c0c0640e7819
Alex Sandberg <alexsandberg7@gmail.com>
2019-08-19 13:36:56 -0600

    Wrap "view details" in button element

--- js/main.js
+++ js/main.js
@@ -156,10 +156,13 @@ createRestaurantHTML = (restaurant) => {
   address.innerHTML = restaurant.address;
   li.append(address);
 
+  const button = document.createElement('BUTTON');
+
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  button.innerHTML = more.outerHTML;
+  li.append(button);
 
   return li
 }
