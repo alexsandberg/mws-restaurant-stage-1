535354aac8c6ebad406cb45fe71a61e49fb4e9bb
Alex Sandberg <alexsandberg7@gmail.com>
2019-08-19 17:01:21 -0600

    Add caching and fetching to service worker

--- sw/service-worker.js
+++ sw/service-worker.js
@@ -1,3 +1,35 @@
-self.addEventListener('fetch', function() {
-	//
+// listens for service worker to finish installing
+self.addEventListener('install', (event) => {
+	// pages to cache
+	const urls = [
+		'/',
+		'/index.html',
+		'/restaurant.html',
+		'/css/styles.css',
+		'/css/responsive.css',
+		'/js/dbhelper.js',
+		'/js/main.js',
+		'/js/restaurant_info.js'
+	];
+
+	// caches the pages once service worker finishes installing
+	event.waitUntil(
+		caches.open('page-cache').then( (cache) => {
+			return cache.addAll(urls);
+		})			
+	)
+})
+
+// listens for fetch events
+self.addEventListener('fetch', (event) => {
+	event.respondWith(
+		caches.match(event.request)
+		.then( (response) => {
+			if(response) {
+				return response; // get page from network if possible
+			}
+			// return the matching response from cache if not connected to network
+			return fetch(event.response); 
+		})
+	)
 })
