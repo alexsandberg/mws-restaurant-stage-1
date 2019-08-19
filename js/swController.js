256267d58e7fc683bac02eb12bcf88ea556d4c62
Alex Sandberg <alexsandberg7@gmail.com>
2019-08-19 17:08:19 -0600

    Adjust comments

--- js/swController.js
+++ js/swController.js
@@ -1,10 +1,4 @@
-
+// register service worker
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('/sw/service-worker.js')
-	.then(function() {
-		console.log('registered!');
-	})
-	.catch(function() {
-		console.log('failed!');
-	})
 }
